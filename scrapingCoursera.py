50488cb08dde86df0dfe6825a00000bc0218a7a9
Bhavya <bhavyapratap0506@gmail.com>
2024-12-11 10:48:42 +0530

    update?

--- scrapingCoursera.py
+++ scrapingCoursera.py
@@ -1,20 +1,15 @@
 import requests
 from dotenv import load_dotenv
 import os
+from urllib.parse import quote
 
 load_dotenv()
 
 search = "NLP"
-limit = 1
-search2 = ""
+limit = 10
+search2 = quote(search)
 
-for i in range(len(search)):
-    if (search[i] == " "):
-        search2 += "%20"
-    else:
-        search2 += search[i]
-
-print("Search2:"+search2)
+print("Search2:", search2)
 
 # Define the API endpoint and your access token
 url = f"https://api.coursera.org/api/courses.v1?q=search&query={search2}&limit={limit}"
@@ -28,6 +23,6 @@ response = requests.get(url, headers=headers)
 # Parse the JSON response
 if response.status_code == 200:
     data = response.json()
-    print(data)
+    print(data['elements'])  # Full response for inspection
 else:
     print(f"Error: {response.status_code}, {response.text}")
